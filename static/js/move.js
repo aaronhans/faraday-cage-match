67cc3340b5865305d5e5a6f28b0ceb9b7621fd61
Aaron Hans <aaronhans@gmail.com>
2013-11-09 13:32:56 -0800

    little better if no rotation while jumping

--- static/js/move.js
+++ static/js/move.js
@@ -20,6 +20,7 @@ function onRelease(event)
 var jumping = false;
 function jump() {
 	console.log('jumpin')
+	rotating = false;
 	falling = false;
 	jumping = true;
 }
@@ -27,6 +28,27 @@ function jump() {
 var falling = false;
 function fall() {
 	console.log('back down please')
+	rotating = false;
 	jumping = false;
 	falling = true;
+}
+
+function letterCollision(lettersInPlay) {
+	
+	for (var i = 0; i < lettersInPlay.length; i++) 
+	{
+		var letter = lettersInPlay[i]
+		if(letter.isPickedUp)continue;
+		var xdist = letter.position.x - steve.position.x;
+		if(xdist > -letter.width/2 && xdist < letter.width/2)
+		{
+			var ydist = letter.position.y - steve.position.y;
+		
+			if(ydist > -letter.height/2 && ydist < letter.height/2)
+			{
+				this.engine.pickupManager.removePickup(i);
+				this.engine.pickup();
+			}
+		}
+	}	
 }
