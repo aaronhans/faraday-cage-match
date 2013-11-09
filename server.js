86251384a8cafe5bbef42ccc15c9473392136fed
Aaron Hans <aaronhans@gmail.com>
2013-11-08 20:22:49 -0800

    will this fix deploy?

--- server.js
+++ server.js
@@ -17,4 +17,25 @@ app.set('view options', {
   layout: false
 });
 
-app.listen(port);
+app.get('*', function(req, res) {
+
+  var voteko = '<iframe src="http://nodeknockout.com/iframe/faraday-cage-match" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<html><body>' + voteko + '</body></html>\n');
+
+});
+
+app.listen(port, function(err) {
+  if (err) { console.error(err); process.exit(-1); }
+
+  // if run as root, downgrade to the owner of this file
+  if (process.getuid() === 0) {
+    require('fs').stat(__filename, function(err, stats) {
+      if (err) { return console.error(err); }
+      process.setuid(stats.uid);
+    });
+  }
+
+  console.log('Server running at http://0.0.0.0:' + port + '/');
+});
