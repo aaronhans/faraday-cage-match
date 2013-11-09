b86773a80aff13cd1d29aba11a2d75a3b9af2561
Aaron Hans <aaronhans@gmail.com>
2013-11-08 20:14:35 -0800

    god bless pixi.js

--- server.js
+++ server.js
@@ -4,23 +4,17 @@ require('nko')('o-BO4moteEDG_Hfb');
 var isProduction = (process.env.NODE_ENV === 'production');
 var http = require('http');
 var port = (isProduction ? 80 : 8000);
+var express = require("express");
+var app = express();
 
-http.createServer(function (req, res) {
-  // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-  var voteko = '<iframe src="http://nodeknockout.com/iframe/faraday-cage-match" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
-
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body>' + voteko + '</body></html>\n');
-}).listen(port, function(err) {
-  if (err) { console.error(err); process.exit(-1); }
-
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) { return console.error(err); }
-      process.setuid(stats.uid);
-    });
-  }
-
-  console.log('Server running at http://0.0.0.0:' + port + '/');
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.use(express.static('./static/'));
+  app.use(app.router);
 });
+app.set('view engine', 'ejs');
+app.set('view options', {
+  layout: false
+});
+
+app.listen(port);
