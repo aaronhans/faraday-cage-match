9e0decc6486b7646dc8b6d5eccc9adaafd11fa8a
Aaron Hans <aaronhans@gmail.com>
2013-11-08 23:07:28 -0800

    letters with tile background

---
+++ static/js/text.js
@@ -0,0 +1,20 @@
+function makeLetter(letter,xPos,yPos,parent) {
+
+	var sprite = PIXI.Sprite.fromImage("img/tile.png");
+	sprite.height = 50
+	sprite.width = 50
+	sprite.position.x = xPos;
+	sprite.position.y = yPos;
+	sprite.anchor.x = 0;
+	sprite.anchor.y = 0;
+
+	var text = new PIXI.Text(letter);
+	text.anchor.x = -0.15;
+	text.anchor.y = 0.05;
+	text.setStyle({font:"bold 128px Arial", fill:"black"});
+
+	sprite.addChild(text)
+
+	parent.addChild(sprite);
+
+}
