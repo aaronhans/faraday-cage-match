6b3457455ac67e79755ac0c81b61b7e6a937edda
Aaron Hans <aaronhans@gmail.com>
2013-11-09 22:13:15 -0800

    allow var height

--- static/js/pieces.js
+++ static/js/pieces.js
@@ -12,14 +12,14 @@ function init() {
 		hero.anchor.x = 0.5;
 		hero.anchor.y = 0.5;
 		hero.position.x = 50;
-		hero.position.y = 232;	
+		hero.position.y = stageHeight - 68;	
 		stage.addChild(hero);
 
 		//all the letters
 		for(var i = 0;i<20;i++) {
 			var letter = alphabet[parseInt(Math.random() * alphabet.length)];
 			var yPos = parseInt(Math.random() * (stageHeight - 200) + 100);
-			var xPos = i * 100; //parseInt(Math.random() * stageWidth * screens);
+			var xPos = i * 100 + 500; //parseInt(Math.random() * stageWidth * screens);
 			lettersInPlay.push(makeLetter(letter,xPos,yPos));
 			stage.addChild(lettersInPlay[lettersInPlay.length -1]);
 		}
@@ -28,7 +28,7 @@ function init() {
 		midTexture = PIXI.Texture.fromImage("img/bg-midsml.png");
 		mid = new PIXI.TilingSprite(midTexture, 512, 256);
 		mid.position.x = 0;
-		mid.position.y = 250;
+		mid.position.y = stageHeight - 50;
 		mid.tilePosition.x = 0;
 		mid.tilePosition.y = 0;
 		stage.addChild(mid);
